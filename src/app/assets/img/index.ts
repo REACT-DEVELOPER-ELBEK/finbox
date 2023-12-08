a0bdeb60fcc719bc6060a55e95015f35716884a9
elbek <elbeky829@gmail.com>
2023-12-09 00:52:00 +0500

    footer added

--- src/app/assets/img/index.ts
+++ src/app/assets/img/index.ts
@@ -1,3 +1,16 @@
-import navLogo from './nav-logo.svg'
+import navLogo from "./nav-logo.svg";
 
-export {navLogo}
+import footerTwitter from "./tiwitter.svg";
+import footerFacebook from "./facebook.svg";
+import footerTelegram from "./telegram.svg";
+import footerInstagram from "./instagram.svg";
+
+import upay from "./upay.svg";
+import payme from "./payme.svg";
+import masterCard from "./mastercard.svg";
+import zoodpay from "./zoodpay.svg";
+import zoodpayLogo from "./zoodpay-logo.svg";
+
+export { navLogo };
+export { footerTwitter, footerFacebook, footerTelegram, footerInstagram };
+export { upay, payme, masterCard, zoodpay, zoodpayLogo };
--- src/app/layout/footer/Footer.tsx
+++ src/app/layout/footer/Footer.tsx
@@ -4,20 +4,84 @@ import Image from "next/image";
 import { navLogo } from "@/app/assets/img";
 import { BsFillTelephoneFill } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
+import {
+  footerTwitter,
+  footerFacebook,
+  footerTelegram,
+  footerInstagram,
+} from "../../assets/img/index";
+import {
+  upay,
+  payme,
+  masterCard,
+  zoodpay,
+  zoodpayLogo,
+} from "../../assets/img/index";
 
 const Footer = () => {
+  const additionalInfos = [
+    "Kurs haqida malumot",
+    "Lorem ipsum",
+    "Trading kurslari",
+    "Yetkazib berish",
+    "Bepul kurslar",
+    "Chegirmalar",
+    "Buyurtma",
+    "Ustozlar",
+  ];
+  const socialMediasLogo = [
+    footerTwitter,
+    footerFacebook,
+    footerTelegram,
+    footerInstagram,
+  ];
+  const paymentItems = [upay, payme, masterCard, zoodpayLogo, zoodpay];
   return (
     <footer>
       <div className="container">
         <div className="footer__wrapper">
           <div className="footer__info">
-            <Image src={navLogo} alt="logo"/>
-            <h2>95 FF3, App Street Avenue NSW 96209</h2>
-            <h3><span></span></h3>
-            <h3><span></span></h3>
+            <Image src={navLogo} alt="logo" className="footer-logo" />
+            <p>95 FF3, App Street Avenue NSW 96209</p>
+            <p>
+              <span>
+                <BsFillTelephoneFill /> Telefon:{" "}
+              </span>
+              8(374) 225 80 00
+            </p>
+            <p>
+              <span>
+                <MdEmail /> Email:{" "}
+              </span>
+              elbeky829@gmail.com
+            </p>
           </div>
-          <div className="footer__course__info"></div>
-          <div className="footer__payment"></div>
+          <div className="footer__course__info">
+            <h1>Qo’shimcha ma’lumot va xizmatlar:</h1>
+            <ul>
+              {additionalInfos.map((item: string, index: number) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="footer__payment">
+            <div className="footer__payment__meets">
+              <h2>Biz bilan bog`laning</h2>
+              <div className="social__media__items">
+                {socialMediasLogo.map((item: string, index: number) => (
+                  <Image src={item} alt="index" />
+                ))}
+              </div>
+            </div>
+            <div className="footer__payment__choices">
+              <h2>To’lovlar:</h2>
+              <div className="footer__payment__choices__items">
+                {paymentItems.map((item: string, index: number) => (
+                  <Image src={item} alt="logo" />
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
